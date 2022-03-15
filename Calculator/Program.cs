910e783089a49eef660a4ad0f82774ec4255b447
Revanescence <jdangagne@gmail.com>
2022-03-15 18:52:12 -0400

    Menu and ability to add input

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,15 +4,86 @@
     {
         static void Main(string[] args)
         {
-            AddNumbers();
+            Console.WriteLine("Welcome to the calculator!");
+
+            while (true)
+            {
+                Console.WriteLine("Select one of the listed funtions");
+                Console.WriteLine("[1] - Add numbers");
+                Console.WriteLine("[2] - Subtract numbers");
+                Console.WriteLine("[3] - Divide numbers");
+                Console.WriteLine("[4] - Multiply numbers");
+                Console.WriteLine("[0] - Exit");
+
+                int menu = int.Parse(Console.ReadLine());
+					
+				switch(menu)
+				{
+					case 0:
+						Console.Clear();
+						Console.WriteLine("Goodbye!");
+						return;
+                    case 1:
+                        AddInput();
+                        break;
+                    case 2:
+                        SubtractInput();
+                        break;
+                    case 3:
+                        DivideInput();
+                        break;
+                    case 4:
+                        MultiplyInput();
+                        break;
+                }
+            }
         }
 
-        static void AddNumbers()
+        static void AddInput()
         {
-            Console.WriteLine("Enter numbers to be added: ");
-            array numbersToAdd= int.Parse(Console.ReadLine()); 
-            Console.WriteLine(numbersToAdd[]);
-            Testing
+            //create array with length equal to quantity of numbers   
+            Console.WriteLine("How many numbers do you want to add?");
+            int totalAmountOfNumbers=int.Parse(Console.ReadLine());
+            int [] numbers = new int[totalAmountOfNumbers];
+
+            //populate array with user input
+            for (int i=0; i<numbers.Length; i++)
+            {
+                if(i==0)
+                {
+                    Console.WriteLine("Enter your first number:");
+                    numbers[i]=int.Parse(Console.ReadLine()); 
+                }
+                else if (i>0)
+                {    
+                    Console.WriteLine("Enter the next number to add:");
+                    numbers[i]=int.Parse(Console.ReadLine());
+                }
+
+            }
+
+            //iterate over array and add the values together
+            int solution=0;
+            for(int c=0; c<numbers.Length; c++)
+            {
+                solution = numbers[c] + solution;
+            }
+
+            Console.WriteLine("The sum of your numbers is: "+solution);
+        }
+
+        static void SubtractInput()
+        {
+
+        }
+        static void DivideInput()
+        {
+
+        }
+
+        static void MultiplyInput()
+        {
+
         }
     }
 }
