a735641f22af40b3f4dfd694ad27b43cb00c1141
Revanescence <jdangagne@gmail.com>
2022-03-15 17:30:04 -0400

    Second Commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers();
+        }
+
+        static void AddNumbers()
+        {
+            Console.WriteLine("Enter numbers to be added: ");
+            array numbersToAdd= int.Parse(Console.ReadLine()); 
+            Console.WriteLine(numbersToAdd[]);
+        }
+    }
+}
