b696c2090d13cc79a97e8c392a9cfed99b833cf4
Jeromgear <jeromegear@gmail.com>
2022-03-17 17:42:01 -0400

    new update

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿namespace Calculator1{
+ class Program { 
+
+     public static void Main(srting [] args ){
+         int a, b, choice;
+
+         Console.WriteLine("Enter the opertion type you wish to perform");
+            
+            switch (choice)
+            {
+              case1:
+              {Console.W}  
+                default:
+            }
+         
+
+
+    }
+
+
+}
