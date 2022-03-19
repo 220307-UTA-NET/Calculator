4f8da66a294f72d5806d32ec9a37558a3a6757fb
Kelly Keng <kelly.keng1@gmail.com>
2022-03-19 17:43:49 -0400

    taking in 2 numbers and operation to perform.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Calculator Project 
+
+Console.WriteLine ("~~~~~~~~  Calculator  ~~~~~~~~ \nPlease Enter Your First Number: ");
+
+string firstNumber = Console.ReadLine();  
+// Create code to use null input to mean "If you'd like to use the calculator, please run program again."
+
+// Takes in operation and decides if valid or tells user to try again 
+Console.WriteLine (" \nPlease Select the operation you'd like to perform. \nOpperation:       Select: \nAddition            +\nSubtaction:         -\nDivision:           %\nMultiplication:     * \n  \nOperation: " );
+string operation = Console.ReadLine();
+
+
+Console.WriteLine (" \nPlease Enter Your Second Number: ");
+string secondNumber = Console.ReadLine();  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
