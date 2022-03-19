ba4ffc5e0a75c7916ba630fd64584c801eea7900
Zhixin He <zhixin.he@hotmail.com>
2022-03-18 23:27:27 -0700

    add handling exceptions

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,10 +9,37 @@ namespace CalculatorProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("please input: ");
-            string strToCalculate = Console.ReadLine();
-            double result = Calculator.Calculate(strToCalculate);
-            Console.WriteLine(result);
+            Console.WriteLine("Welcome, This is a One-Line Arithmetic Calculator.\n\n");
+            Console.WriteLine("The following arithmetic operators are supported:");
+            Console.WriteLine("         +   Add");
+            Console.WriteLine("         -   Subtract");
+            Console.WriteLine("         *   Multiple");
+            Console.WriteLine("         /   Divide");
+            Console.WriteLine("         **  Exponent");
+            Console.WriteLine("\n   Hint: Use parentheses () to explicitly show your priority.");
+            Console.WriteLine("\n   Quit: Q/q");
+            while (true)
+            {
+                Console.WriteLine("Please input your arithmetic expression:");
+                string strUserInput = Console.ReadLine();
+                if (strUserInput == "Q" || strUserInput == "q")
+                {
+                    Console.WriteLine("BYE.");
+                    break;
+                }
+                try {
+                    Console.WriteLine($"The result: {Calculator.Calculate(strUserInput)}");
+                } catch ( Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("OOPs, Please try again.");
+                    // error logging 
+                    continue;
+                }
+
+                // logging here
+            }
+            
         }
 
     }
