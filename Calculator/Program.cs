f28c5eb773239a5dddb2d3857fb6bc1d49a31e3c
bruk202 <40613642+bruk202@users.noreply.github.com>
2022-03-20 19:28:53 -0400

    First commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Web;
+
+namespace practice
+{
+    class inputInt
+    {
+         static void Main(string[] args)
+        
+        {
+            Console.WriteLine("Enter your number");
+            Console.WriteLine("----------");
+            int N = Int32.Parse(Console.ReadLine());
+           
+            Console.WriteLine("Enter your second number");
+           int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("----------");
+             int l = N + n;
+             int h = N - n;
+
+             Console.WriteLine(" Please choose your operations number");
+             Console.WriteLine("   1 -> Addision\n   2 -> Sunstraction\n   3 -> multpilication \n   4 -> Division");
+            int input = Int32.Parse(Console.ReadLine());
+
+             if (input == 1)
+            {
+                 Console.WriteLine(N +" + " + n + " = " + l);
+            }
+                 else if (input == 2)
+            {
+                  Console.WriteLine(N + " - " + n + " = " + h);
+                    
+            }
+              else if (input == 3)
+            {
+                 
+                    Console.WriteLine( N + " / " + n + " = " + N * n);
+            }
+                
+              else if  (input == 4)
+            {
+                    Console.WriteLine( N + " / " + n + " = " + N / n);
+                    
+            }
+              else
+            {
+                 Console.WriteLine("Invallid input");
+            }
+             
+    
+        }
+        
+    }
+}
+
+
+
