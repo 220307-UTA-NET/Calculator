b541259f13062611e3558b8139465c717f207c78
Kevin Lee <klee5760@hotmail.com>
2022-03-21 09:10:29 -0500

    Updated 03-21-2022

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,16 +10,46 @@
         {
             Console.WriteLine("Select the operator [1]Addition [2]Subtraction [3]Multiplcation [4]Division [0]To Exit"); 
 
-            char op;
-            op = Console.ReadLine()[0];   
+            int userMenuInput = int.Parse(Console.ReadLine());
+
+            switch (userMenuInput)
+            {
+                case 1:
+                Addition();
+                break;
+
+                case 2:
+                Subtraction();
+                break;
+
+                case 3:
+                Multiplcation();
+                break;
+
+                case 4:
+                Division();
+                break;
+
+                case 0:
+                userOperate = false;
+                Environment.Exit(0);
+                break;
+
+                default:
+                Console.WriteLine("Invalid Input! Please type the correct input!");
+                break;
+            }
+
+            //char op;
+            //op = Console.ReadLine()[0];   
                     
-            Console.WriteLine("Enter the two numbers one by one");
+           /* Console.WriteLine("Enter the two numbers one by one");
             Console.WriteLine("Enter the first number");
             double num1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter the second number");
-            double num2 = Convert.ToDouble(Console.ReadLine());           
+            double num2 = Convert.ToDouble(Console.ReadLine());    */       
 
-            switch(op)
+            /*switch(op)
             {
                 case '1':
                     Console.WriteLine("{0} + {1} = {2}", num1, num2, (num1 + num2));
@@ -47,10 +77,50 @@
                 default:
                     Console.WriteLine("Invalid Input! Please type the correct input!", op);
                     break;
-            }
+            }*/
                
            // Console.WriteLine("Do you wish you want to continue? Press [1] To continue or press any key to exit"); 
 
         }while(userOperate == true);            
     }  
+
+    static void Addition()
+    {
+        Console.WriteLine("Enter the first number");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the second number");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("{0} + {1} = {2}", num1, num2, (num1 + num2));
+    }
+
+    static void Subtraction()
+    {
+        Console.WriteLine("Enter the first number");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the second number");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("{0} - {1} = {2}", num1, num2, (num1 - num2));
+    }
+
+    static void Multiplcation()
+    {
+        Console.WriteLine("Enter the first number");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the second number");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("{0} * {1} = {2}", num1, num2, (num1 * num2));
+    }
+
+    static void Division()
+    {
+        Console.WriteLine("Enter the first number");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the second number");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        if(num2 == 0.0)
+        Console.WriteLine("Denominator cannot divide by zero (0)!");
+        else
+        Console.WriteLine("{0} / {1} = {2}", num1, num2, (num1 / num2));
+    }
 }
