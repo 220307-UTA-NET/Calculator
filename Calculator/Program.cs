67ff3ec0cd90657e9ac65e3f78630097f4f4f3ba
bruk202 <40613642+bruk202@users.noreply.github.com>
2022-03-21 10:06:10 -0400

    Final code

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,29 +1,38 @@
 ﻿using System;
 
 
-namespace practice
+namespace Project_1_Calculator
 {
-    class InputInt
+    class calculator
     {
    
-    
-         static void Main(string[] args)
-        
+             static void Main(string[] args)
+         
         {
+          
+             try{
             Console.WriteLine("Enter your number");
             Console.WriteLine("----------");
-            int N = Int32.Parse(Console.ReadLine());
+            double N = Int32.Parse(Console.ReadLine());
            
-            Console.WriteLine("Enter your second number");
-           int n = Int32.Parse(Console.ReadLine());
+           Console.WriteLine("Enter your second number");
+           double n = Int32.Parse(Console.ReadLine());
             Console.WriteLine("----------");
-             int l = N + n;
-             int h = N - n;
+             double l = N + n;
+             double h = N - n;
 
-             Console.WriteLine(" Please choose your operations number");
-             Console.WriteLine("   1 -> Addision\n   2 -> Substraction\n   3 -> multpilication \n   4 -> Division");
-            int input = Int32.Parse(Console.ReadLine());
+          Console.WriteLine("Please choose your operations number");
+          Console.WriteLine("   1 -> Addision\n   2 -> Substraction\n   3 -> multpilication \n   4 -> Division");
+           
+              Console.WriteLine("You chose ");
+              int input = Int32.Parse(Console.ReadLine());
+              
+               
+                   
 
+          
+                    
+               
              if (input == 1)
             {
                  Console.WriteLine(N +" + " + n + " = " + l);
@@ -36,24 +45,36 @@ namespace practice
               else if (input == 3)
             {
                  
-                    Console.WriteLine( N + " / " + n + " = " + N * n);
+                    Console.WriteLine( N + " X " + n + " = " + N * n);
             }
                 
               else if  (input == 4)
             {
-                    Console.WriteLine( N + " / " + n + " = " + N / n);
+                    Console.WriteLine( N + " / " + n + " = " + (double)N / (double) n);
                     
             }
               else
             {
-                 Console.WriteLine("Invallid input");
+                 Console.WriteLine("Invalid input ");
             }
-             
+             }
+             catch(Exception e)
+             {
+               
+                    Console.WriteLine(e.Message);
+               }
+
+             }
+             //Console.WriteLine("Enter to continue");
+
+            // Console.ReadLine();
+           //  Console.Clear();
+    
     
         }
         
     }
-}
+
 
 
 
