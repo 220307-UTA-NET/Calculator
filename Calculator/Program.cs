bb8e538b15ec24f820d33d133321a127644b8d3a
AnuAnil43 <101119895+AnuAnil43@users.noreply.github.com>
2022-03-21 22:46:09 -0400

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,85 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more in
+//Be written in C# for .NET 6
+//Be able to accept multiple numbers
+//Perform a selected operation on those numbers
+//Display the result of the operation
+//Repeat until the user chooses to exit
+using System;
+namespace  Calculator
+{
+public class Program {
+
+static void myCalc()
+    {
+     double number;
+     double result = 0;
+     string input = "0";
+     string operation = "";
+     while(!input.Equals ("=") )
+      {
+       input = Console.ReadLine();
+       if(double.TryParse(input,out number))  //main if
+       {
+               if(operation =="")
+               { 
+                    result = number;
+                    
+               }
+               else if(operation =="+")
+               {
+                     result += number;
+                  
+               }
+               else if(operation =="-")
+               {
+                    result -= number;
+                
+               }
+               else if(operation =="*")
+               {
+                     result *= number;
+                    
+               }
+               else if(operation =="/")
+               {
+                     result /= number;
+                 
+               }
+               
+       }
+      else if (input.Equals("+") || input.Equals("-") || input.Equals("*") || input.Equals("/"))  //main if
+               {
+               operation = input ;
+               } 
+     }
+         Console.WriteLine("Total = " +result);
+     }               
+
+  public static void Main(string[] args)
+    {
+     Console.WriteLine("Calculator Project \n\nTo Add(+),Subtract(-),Multiply(*),Divide(/)");
+     Console.WriteLine("\nEnter the number followed by operator, =(equal) sign will print the result");
+     ConsoleKeyInfo keyInfo;
+     myCalc();
+     while(true)
+     {
+         Console.WriteLine("Do you want to continue:(Y/N)");
+         keyInfo = Console.ReadKey();
+        if(keyInfo.Key == ConsoleKey.Y)
+        {
+               Console.WriteLine("\nEnter the number followed by operator, =(equal) sign will print the result");
+               myCalc();
+        }
+        else if(keyInfo.Key == ConsoleKey.N)
+        {
+            return;
+        }
+        else
+        Console.WriteLine("\nError - Give Y or N to continue");
+     }
+    }
+}
+
+}
+
